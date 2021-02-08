e31b467a4e9dae54bbfa8a254643e83be632d1ec
Baniene <76951522+Baniene@users.noreply.github.com>
2021-02-08 10:12:53 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import { Todo } from './Todo.js';
+
+const randomUzduotys = new Todo('Random uzduotys');
+// const namuDarbai = new Todo('Namu darbai');
+
+console.log(randomUzduotys.summary());
+//console.log(namuDarbai.summary());
+
+
+
